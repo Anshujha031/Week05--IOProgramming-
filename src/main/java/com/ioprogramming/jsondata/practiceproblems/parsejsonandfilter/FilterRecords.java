4b321eb7a6fdfc28505b95d32f3df3db55f36125
naveenjha031 <naveenjha344@gmail.com>
2025-02-19 21:27:28 +0530

    [Naveen] Add. Parse json and Filter

---
+++ src/main/java/com/ioprogramming/jsondata/practiceproblems/parsejsonandfilter/FilterRecords.java
@@ -0,0 +1,28 @@
+package com.ioprogramming.jsondata.practiceproblems.parsejsonandfilter;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import java.io.File;
+
+public class FilterRecords {
+    public static void main(String[] args) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonArray = objectMapper.readTree(new File("F:\\CG-Training\\week05-IoProgramming\\Dat02\\src\\main\\java\\com\\ioprogramming\\jsondata\\practiceproblems\\parsejsonandfilter\\user.json"));
+
+            ArrayNode flterArray = objectMapper.createArrayNode();
+
+            for(JsonNode node : jsonArray){
+                if(node.get("age").asInt()>25){
+                    flterArray.add(node);
+                }
+            }
+            String filterJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(flterArray);
+            System.out.println(filterJson);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
